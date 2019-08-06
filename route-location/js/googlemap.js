65c1c76da9ab0b692648522855fb443b2a982700
KenNguch <kennguch3@gmail.com>
2019-08-06 17:01:32 +0300

    Zoom Level Added

--- route-location/js/googlemap.js
+++ route-location/js/googlemap.js
@@ -7,7 +7,7 @@ function loadMap() {
         lng: 36.626874
     };
     map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 12,
+        zoom: 24,
         center: pune
     });
 
