067fb63cf63121a640126a7eefc3437d70fb7190
ValentineMwaura <bobitlmr567717@spu.ac.ke>
2019-08-06 17:59:56 +0300

    changed Header

--- header-user.php
+++ header-user.php
@@ -28,7 +28,7 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="index.html">Garbage Kollectors</a>
+                <a class="navbar-brand" href="index.html">Garbage kollectorz</a>
             </div>
 
             <ul class="nav navbar-top-links navbar-right">
