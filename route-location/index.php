73fd7dfd5da535a32e5bee77e7d72063515e121b
ValentineMwaura <48595340+ValentineMwaura@users.noreply.github.com>
2019-08-12 17:38:32 +0300

    Update index.php

--- route-location/index.php
+++ route-location/index.php
@@ -18,26 +18,7 @@
 		}
 	</style>
 </head>
-<body>
-	<button onclick="history.go(-1);">Back </button>
-	<div class="container">
-		<?php 
-			require 'education.php';
-			$edu = new education;
-			$coll = $edu->getlocationsBlankLatLng();
-			$coll = json_encode($coll, true);
-			echo '<div id="data">' . $coll . '</div>';
-			$allData = $edu->getAlllocations();
-			$allData = json_encode($allData, true);
-			echo '<div id="allData">' . $allData . '</div>';			
-		 ?>
-		<div id="map"></div>
-	</div>
-	<button onclick="history.go(-1);">Forward </button>
-</body>
-<script async defer
-      src="https://maps.googleapis.com/maps/api/js?key=AIzaSyA1bF3Ry-gVyKmSVse4s1zmfnyd4_9b3F8&callback=loadMap">
-</script>
+
 </html>
 
 </html>
