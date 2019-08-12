73f9bc2b6e4eac752186833c88f8096c770bcd8d
ValentineMwaura <48595340+ValentineMwaura@users.noreply.github.com>
2019-08-12 18:43:31 +0300

    Update index.php
    Copy pasted code

--- route-location/index.php
+++ route-location/index.php
@@ -1,31 +1,46 @@
-<!DOCTYPE html>
 <html>
 <head>
-	<title></title>
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<title>Route Map Allocation</title>
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<script type="text/javascript" src="js/jquery-3.2.1.min.js"></script>
+	<script type="text/javascript" src="js/googlemap.js"></script>
+	<style type="text/css">
+		.container {
+			height: 600px;
+		}
+		#map {
+			width: 100%;
+			height: 100%;
+			border: 1px solid blue;
+		}
+		#data, #allData {
+			display: none;
+		}
+	</style>
 </head>
 <body>
-<div class="container">
-	<div class="row">
-		<div class="col-md-8 col-md-offset-2">
-			<h3 class="text-center page-header">Google Map Location</h3>
-			<div id="map" style="height: 400px; width: 100%;"></div>
-		</div>
-	</div>
+<div> 
+<button onclick="history.go(-1);">Back </button>
 </div>
-<script>
-function initMap() {
-   	var location = {lat:-1.1136, lng:36.6420517};
-   	var map = new google.maps.Map(document.getElementById('map'), {
-      	zoom: 12,
-      	center: location
-    });
-    var marker = new google.maps.Marker({
-      	position: location,
-      	map: map
-    });
-}
-</script>
-<script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyA1bF3Ry-gVyKmSVse4s1zmfnyd4_9b3F8&callback=initMap"></script>
+	<div class="container">
+		<?php 
+			require 'education.php';
+			$edu = new education;
+			$coll = $edu->getlocationsBlankLatLng();
+			$coll = json_encode($coll, true);
+			echo '<div id="data">' . $coll . '</div>';
+			$allData = $edu->getAlllocations();
+			$allData = json_encode($allData, true);
+			echo '<div id="allData">' . $allData . '</div>';			
+		 ?>
+		<div id="map"></div>
+	</div>
+	<div>
+	<button onclick="history.go(1);">Forward </button>
+	</div>
+
 </body>
+<script async defer
+      src="https://maps.googleapis.com/maps/api/js?key=AIzaSyA1bF3Ry-gVyKmSVse4s1zmfnyd4_9b3F8&callback=loadMap">
+</script>
 </html>
