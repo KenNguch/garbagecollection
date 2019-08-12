a3e699a1e8c5006da1cc133d2f4bc71ea83986a5
ValentineMwaura <48595340+ValentineMwaura@users.noreply.github.com>
2019-08-12 13:41:11 +0300

    Update index.php

--- route-location/index.php
+++ route-location/index.php
@@ -34,7 +34,7 @@
 	</div>
 </body>
 <script async defer
-      src="https://maps.googleapis.com/maps/api/js?key=&callback=loadMap">
+      src="https://maps.googleapis.com/maps/api/js?key=AIzaSyA1bF3Ry-gVyKmSVse4s1zmfnyd4_9b3F8&callback=loadMap">
 </script>
 </html>
 
